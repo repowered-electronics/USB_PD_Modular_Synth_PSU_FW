0e19617944b23fbe380577166f9590405c8f50d3
Adam Cordingley <adam@repoweredelectronics.com>
2025-04-29 02:00:24 +0000

    squash

--- Core/Inc/ina236.h
+++ Core/Inc/ina236.h
@@ -4,10 +4,10 @@
   * @brief          : INA236 header file
   ******************************************************************************
   * @attention
-  * Copyright (C) 2024  Repowered Electronics LLC
+  * Copyright (C) 2025  Repowered Electronics LLC
   *
   * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
+  * it under the terms of the GNU Lesser General Public License as published by
   * the Free Software Foundation, either version 3 of the License, or
   * (at your option) any later version.
   *
